32666da30ce514b41aa0bef52d948d6ab19d7876
Justin Trujillo <jtrujillo923@gmail.com>
2022-02-08 09:42:01 -0700

    Initial MTR Copy

--- main.py
+++ main.py
@@ -3,6 +3,68 @@ from icmplib import ping, multiping, traceroute, resolve
 from icmplib import async_ping, async_multiping, async_resolve
 from mac_vendor_lookup import MacLookup
 import ipaddress
+import asyncio
+
+
+
+
+
+async def async_mtr(ips, hop_info):
+    mtr_results = {}
+    for ip in ips:
+        mtr_results[ip] = {}
+
+    print("Initializing MTR...")
+    for ip in ips:  # Initialize
+        mtr_results[ip]['address'] = 0
+        mtr_results[ip]['rtt'] = 0
+        mtr_results[ip]['packets_sent'] = 0
+        mtr_results[ip]['packets_received'] = 0
+        mtr_results[ip]['packet_loss'] = 0
+        mtr_results[ip]['jitter'] = 0
+
+    from prettytable import PrettyTable
+    pingnum = 1
+    while True:
+        await asyncio.sleep(0.25)
+        response = await async_multiping(ips, count=1)
+        print(f"\n\n\n\n\n")
+        mtr_table = PrettyTable(['IP', 'Address', 'RTT', 'Packets Sent', 'Packets Received', 'Packet Loss', 'Jitter'])
+        for host in response:
+            ip = host.address
+            mtr_results[ip]['address'] = hop_info[ip]['address']
+            mtr_results[ip]['rtt'] = host.avg_rtt
+            mtr_results[ip]['packets_sent'] = mtr_results[ip]['packets_sent'] + host.packets_sent
+            mtr_results[ip]['packets_received'] = mtr_results[ip]['packets_received'] + host.packets_received
+            mtr_results[ip]['packet_loss'] = mtr_results[ip]['packet_loss'] + host.packet_loss
+            mtr_results[ip]['jitter'] = (mtr_results[ip]['jitter'] + host.jitter) / pingnum
+            mtr_table.add_row([ip, mtr_results[ip]['address'], mtr_results[ip]['rtt'], mtr_results[ip]['packets_sent'],
+                               mtr_results[ip]['packets_received'], mtr_results[ip]['packet_loss'],
+                               mtr_results[ip]['jitter']])
+
+            # print(f"{ip} - {pingnum}"
+            #       f"\tRTT = {mtr_results[ip]['rtt']}"
+            #       f"\tPackets Sent = {mtr_results[ip]['packets_sent']}"
+            #       f"\tPackets Received = {mtr_results[ip]['packets_received']}"
+            #       f"\tPacket Loss = {mtr_results[ip]['packet_loss']}"
+            #       f"\tJitter = {mtr_results[ip]['jitter']}")
+        print(mtr_table)
+        pingnum += 1
+
+        # print(f"\t\tmtr\t\t{mtr_results}\n")
+
+
+def mtr(ips, hop_info):
+    asyncio.run(async_mtr(ips, hop_info))
+    # hosts = multiping(ips)
+    # for host in hosts:
+    #     if host.is_alive:
+    #         print(f"{host.address} is up!")
+    #     else:
+    #         print(f"{host.address} is down!")
+
+
+
 
 
 def get_address(myconn):
@@ -47,24 +109,27 @@ def get_mac(myconn):
     #     return "Error"
 
 
-def route_print(ip_hops, routers):
+def route_print(hop_info, routers):
     j = 1
-    for ip in ip_hops:
+    for ip in hop_info:
         print(f"Hop {j} = {ip}\n"
-              f"\tAddress = {ip_hops[ip]['address']}\n"
-              f"\tMBU Version = {ip_hops[ip]['version']}\n"
-              f"\tRouter MAC = {ip_hops[ip]['router']}\n"
+              f"\tAddress = {hop_info[ip]['address']}\n"
+              f"\tMBU Version = {hop_info[ip]['version']}\n"
+              f"\tRouter MAC = {hop_info[ip]['router']}\n"
               f"\tRouter OUI = {routers[ip]}")
         j += 1
 
 
-def gather_route(hops, prefix, myconn, ip_hops=None, routers=None, user="root", pswd="admin", port=22):
+def gather_route(traceroute_hops, prefix, myconn, ip_list,
+                 hop_info=None, routers=None, user="root", pswd="admin", port=22):
     if routers is None:
         routers = {}
-    if ip_hops is None:
-        ip_hops = {}
+    if hop_info is None:
+        hop_info = {}
+    if ip_list is None:
+        ip_list = []
     i = 1
-    for hop in hops:
+    for hop in traceroute_hops:
         # if i < 5:  # ignoring the first two hops
         #     i += 1
         #     continue
@@ -73,22 +138,24 @@ def gather_route(hops, prefix, myconn, ip_hops=None, routers=None, user="root", 
             i += 1
             continue
         try:
+            if ip not in ip_list:
+                ip_list.append(ip)
             # print(ip)
             session = myconn.connect(ip, username=user, password=pswd, port=port)
-            ip_hops[ip] = {}
-            ip_hops[ip]["address"] = get_address(myconn)
-            ip_hops[ip]["router"] = get_mac(myconn)
-            ip_hops[ip]["version"] = get_version(myconn)
+            hop_info[ip] = {}
+            hop_info[ip]["address"] = get_address(myconn)
+            hop_info[ip]["router"] = get_mac(myconn)
+            hop_info[ip]["version"] = get_version(myconn)
             myconn.close()
-            if ip_hops[ip]['router'] == "\'":
+            if hop_info[ip]['router'] == "\'":
                 routers[ip] = "No Router"
             else:
-                routers[ip] = MacLookup().lookup(ip_hops[ip]['router'])
+                routers[ip] = MacLookup().lookup(hop_info[ip]['router'])
 
             # print(f"Hop {j} = {ip}\n"
-            #       f"\tAddress = {ip_hops[ip]['address']}\n"
-            #       f"\tMBU Version = {ip_hops[ip]['version']}\n"
-            #       f"\tRouter MAC = {ip_hops[ip]['router']}\n"
+            #       f"\tAddress = {hop_info[ip]['address']}\n"
+            #       f"\tMBU Version = {hop_info[ip]['version']}\n"
+            #       f"\tRouter MAC = {hop_info[ip]['router']}\n"
             #       f"\tRouter OUI = {routers[ip]}")
             # j += 1
         # print(f"Hop {i} = {hop.address} --- "
@@ -98,9 +165,9 @@ def gather_route(hops, prefix, myconn, ip_hops=None, routers=None, user="root", 
         except Exception as e:
             print(e)
             return "Error"
-        # ip_hops[hop.address] = {}
-        # ip_hops[hop.address]["address"] = get_address(hop.address)
-        # ip_hops[hop.address]["router"] = get_mac(hop.address)
+        # hop_info[hop.address] = {}
+        # hop_info[hop.address]["address"] = get_address(hop.address)
+        # hop_info[hop.address]["router"] = get_mac(hop.address)
         i += 1
 
 
@@ -124,14 +191,15 @@ def main():
 
     myconn = paramiko.SSHClient()
     myconn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    hops = traceroute(target_ip)
+    traceroute_hops = traceroute(target_ip)
     # print('Distance/TTL    Address    Average round-trip time')
     # last_distance = 0
     # i = 1
     # j = 1
-    ip_hops = {}
+    hop_info = {}
     routers = {}
-    gather_route(hops, prefix, myconn, ip_hops, routers)
+    ip_list = []
+    gather_route(traceroute_hops, prefix, myconn, ip_list, hop_info, routers)
 
     while True:
         try:
@@ -146,22 +214,24 @@ def main():
             choice = int(input("Choice: "))
             if choice == 1:
                 print("\n\n")
-                for ip in ip_hops:
+                for ip in hop_info:
                     print(ip)
             elif choice == 2:
                 print("\n\n")
-                for ip in routers:
-                    print(ip)
+                for router in routers:
+                    print(routers[router])
             elif choice == 3:
                 print("\n\n")
-                for ip in ip_hops:
-                    print(ip_hops[ip]['version'])
+                i = 1
+                for ip in hop_info:
+                    print(f"{i}: {hop_info[ip]['version']}")
+                    i += 1
             elif choice == 4:
                 print("\n\n")
-                # mtr(ip_hops)
+                mtr(ip_list, hop_info)
             elif choice == 5:
                 print("\n\n")
-                route_print(ip_hops, routers)
+                route_print(hop_info, routers)
             elif choice == 6:
                 break
             else:
@@ -169,18 +239,18 @@ def main():
         except ValueError:
             print("Invalid Choice")
     # routers = {}
-    # for ip in ip_hops:
-    #     if ip_hops[ip]['router'] == "\'":
+    # for ip in hop_info:
+    #     if hop_info[ip]['router'] == "\'":
     #         routers[ip] = "No Router"
     #     else:
-    #         routers[ip] = MacLookup().lookup(ip_hops[ip]['router'])
+    #         routers[ip] = MacLookup().lookup(hop_info[ip]['router'])
     # j = 1
-    # for ip in ip_hops:
-    #     print(f"Hop {j} = {ip}\t---\t{ip_hops[ip]['address']}\n"
-    #           f"\tRouter = {ip_hops[ip]['router']}\t---\t{routers[ip]}\n")
+    # for ip in hop_info:
+    #     print(f"Hop {j} = {ip}\t---\t{hop_info[ip]['address']}\n"
+    #           f"\tRouter = {hop_info[ip]['router']}\t---\t{routers[ip]}\n")
     #     j += 1
-    # print(ip_hops)
-    # print(ip_hops["fd8d:1900:2729:7415::1"])
+    # print(hop_info)
+    # print(hop_info["fd8d:1900:2729:7415::1"])
 
 
 if __name__ == '__main__':
