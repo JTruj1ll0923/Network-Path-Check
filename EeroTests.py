95fa1c0e3e278005f3510fa29ad96b715fe514bb
JTruj1ll0923 <jtrujillo923@gmail.com>
2022-04-05 16:31:43 -0600

    Code cleanup to prepare for documentation comments

--- EeroTests.py
+++ EeroTests.py
@@ -3,8 +3,6 @@ import asyncio
 import aiohttp
 import json
 from prettytable import PrettyTable
-import pandas as pd
-import openpyxl
 import datetime
 import arrow
 import sys
@@ -22,13 +20,13 @@ try:
         "X-User-Token": user_token
     }
 except Exception as e:
+    print("Error: " + str(e))
     print("Could not load secrets.json for API key")
     user_token = None
     while True:
         c = input("Would you like to enter your credentials manually? (y/n)")
         if c == 'y' or c == 'Y':
             user_token = input("Enter your user token: ")
-            # print("Would you like to save this token to secrets.json? (y/n)")
             while True:
                 c = input("Would you like to save this token to secrets.json? (y/n)")
                 if c == 'y' or c == 'Y':
@@ -39,6 +37,7 @@ except Exception as e:
                         print("Saved!")
                         break
                     except Exception as e:
+                        print("Error: " + str(e))
                         print("Could not save to secrets.json")
                         break
                 elif c == 'n' or c == 'N':
@@ -46,8 +45,6 @@ except Exception as e:
                     break
                 else:
                     print("Invalid input")
-            # with open('secrets.json', 'w') as outfile:
-            #     json.dump({'user_token': user_token}, outfile)
             break
         elif c == 'n' or c == 'N':
             print("Press any key to exit")
@@ -63,10 +60,9 @@ except Exception as e:
         "X-User-Token": user_token
     }
     base_url = "https://api-user.e2ro.com/2.2"
-    # sys.exit(1)
 
 
-def slow_day_report(base_url, headers):
+def slow_day_report():
     report_url = f"{base_url}/organizations/self/slow_network_counts_by_day"
     start = input("Enter start date (YYYY-MM-DD): ")
     print(start)
@@ -89,14 +85,14 @@ def format_tests(tests):
         date = arrow.get(date)
         try:
             date = date - datetime.timedelta(hours=6)
-        except Exception as e:
-            print(f"{e} -- Unable to convert time")
+        except Exception as err:
+            print(f"{err} -- Unable to convert time")
         test_results[i]['date'] = date
         i += 1
     return test_results
 
 
-async def speed_test_list(base_url, headers, customer_id=None):
+async def speed_test_list(customer_id=None):
     if customer_id is None:
         while True:
             try:
@@ -108,16 +104,16 @@ async def speed_test_list(base_url, headers, customer_id=None):
                 continue
     network_url = f"{base_url}/networks/{customer_id}/speedtest"
     data = {"limit": 100}
-    # print(data)
     result = requests.get(network_url, headers=headers, params=data)
-    # print(result.json())
-    result = result.json()
-    # result = json.loads(result)
-    return result
-    # return result.json()
+    if result.status_code == 200:
+        result = result.json()
+        return result
+    else:
+        print(f"Error: {result.status_code}")
+        return None
 
 
-def list_networks(base_url, headers, params=None):
+def list_networks(params=None):
     networks_url = f"{base_url}/organizations/self/networks/administered"
     if params is None:
         result = requests.get(networks_url, headers=headers)
@@ -127,10 +123,9 @@ def list_networks(base_url, headers, params=None):
     return result
 
 
-async def grab_eeros(base_url=base_url, headers=headers):
+async def grab_eeros():
     network_list = []
-    networks = list_networks(base_url, headers)
-    # print(list_networks(base_url, headers)['data']['networks'])
+    networks = list_networks()
     i = 1
     print("Grabbing network page", end=":\n")
     while True:
@@ -145,19 +140,16 @@ async def grab_eeros(base_url=base_url, headers=headers):
                 offset = networks['pagination']['next']
                 offset = offset.split('?')[1]
                 offset = offset.split('=')[1]
-                networks = list_networks(base_url, headers, params={"offset": offset})
+                networks = list_networks(params={"offset": offset})
             else:
-                # print("That's all folks!")
                 break
         except KeyError:
             print("That's all folks!")
             break
-    df = pd.DataFrame()
     i = 1
     network_dict = {}
     router_dict = {}
     time = arrow.now().format('YYYY-MM-DD_HH:mm:ss')
-    # network_dict['date'] = time
     router_dict['date'] = time
     for network in network_list:  # Combine all Eero network dictionaries into one dictionary
         network_dict[i] = {}
@@ -175,28 +167,24 @@ async def grab_eeros(base_url=base_url, headers=headers):
         network_dict[i]['eeros'] = []
         for eero in network['eeros']:  # Gathering list of routers and associated account url (allow searching for mac)
             network_dict[i]['eeros'].append(eero)
-            # print(f"Eero\t---\t{eero}")
             router_dict[eero['mac_address']] = {
                 'serial': eero['serial'],
                 'url': network['url'],
             }
         i += 1
 
-    # df = pd.DataFrame.from_dict(network_dict, orient='index')
-    # df.to_csv('networks.xlsx', index=False)
-    df = pd.DataFrame.from_dict(router_dict, orient='index')
-    # df.to_csv('routers.xlsx', index=False)
     routers = open('routers.json', 'w')
     routers.write(json.dumps(router_dict))
     routers.close()
-    # df.to_json('routers.json', orient='index')
     return "That's all folks!"
 
 
-async def single_eero_results(base_url=base_url, headers=headers, customer_id=None):
-    # print("Grabbing Eero results for customer ID: {}".format(customer_id))
-    tests = await speed_test_list(base_url, headers, customer_id)
-    # print(f"Grabbed Eero result for customer ID: {customer_id}\t---\t{tests}")
+async def single_eero_results(customer_id=None):
+    if customer_id is None:
+        return
+    tests = await speed_test_list(customer_id)
+    if tests is None:
+        return "No results found"
     test_results = format_tests(tests)
     table = PrettyTable(["Num", "Date", "Down", "Up"])
     i = 1
@@ -206,33 +194,26 @@ async def single_eero_results(base_url=base_url, headers=headers, customer_id=No
     return table
 
 
-def search_by_mac(base_url=base_url, headers=headers, mac=None):
+def search_by_mac(mac=None):
     if mac is None:
-        # mac = input("Enter the MAC address of the router you want to search for: ")
         return "No MAC address entered"
     mac = mac[:-1].strip() + '0'
-    data = None
     try:
         with open('routers.json') as json_file:
             data = json.load(json_file)
-            id = data[mac]['url']
-            # print(data)
-            id = id.split('/')
-            id = id[len(id) - 1]
-            # print(mac)
+            customer_id = data[mac]['url']
+            customer_id = customer_id.split('/')
+            customer_id = customer_id[len(customer_id) - 1]
             serial = data[mac]['serial']
-            # print(serial)
-            # data = f"https://dashboard.eero.com/networks/{data}"
         json_file.close()
-        return id, serial
-    except Exception as e:
-        # print("No network with that MAC address found.")
+        return customer_id, serial
+    except Exception as err:
+        print(err)
         return "Missing Network", "Missing Serial"
 
 
-def search_by_serial(base_url, headers):
+def search_by_serial():
     serial = input("Enter the serial of the router you want to search for: ")
-    data = None
     try:
         with open('routers.json') as json_file:
             data = json.load(json_file)
@@ -240,14 +221,15 @@ def search_by_serial(base_url, headers):
             data = data.split('/')
             data = data[len(data) - 1]
         json_file.close()
-        result = asyncio.run(single_eero_results(base_url, headers, data))
+        result = asyncio.run(single_eero_results(data))
         return result
-    except Exception as e:
+    except Exception as err:
+        print(err)
         print("No router with that MAC address found.")
         return
 
 
-async def async_tasker(session, base_url, headers, customer_id):
+async def async_tasker(session, customer_id):
     result = None
 
     while True:
@@ -256,44 +238,34 @@ async def async_tasker(session, base_url, headers, customer_id):
             data = {'limit': 100}
             result = await session.get(url=network_url, headers=headers, params=data)
             if result is not None:
-            # result = result.json()
-            # print(result)
-            # async with session.get(url=network_url, headers=headers, params=data) as resp:
-            #     if resp is not None:
-                    # print(f"Successfully grabbed results from these variables {customer_id}")
-                    # print(f"{resp.json()}")
-
                 break
             else:
                 print("No results found.")
                 break
-        except Exception as e:
-            print(f"Error: {e}")  # Add possible error retry logic here
+        except Exception as err:
+            print(f"Error: {err}")  # Add possible error retry logic here
             continue
-    # return resp
     return result
 
 
-def get_tasks(session, base_url, headers):
+def get_tasks(session):
     tasks = []
     with open('routers.json') as json_file:
         data = json.load(json_file)
-        # print(data)
         for router in data:
             url = data[router]['url']
             url = url.split('/')
             url = url[len(url) - 1]
-            # print(url)
-            tasks.append(asyncio.create_task(async_tasker(session, base_url, headers, url)))
+            tasks.append(asyncio.create_task(async_tasker(session, url)))
         json_file.close()
     return tasks
 
 
-async def async_runner(base_url, headers):
+async def async_runner():
     results = []
     sem = asyncio.Semaphore(100)
     async with sem, aiohttp.ClientSession() as session:
-        tasks = get_tasks(session, base_url, headers)
+        tasks = get_tasks(session)
         responses = await asyncio.gather(*tasks)
         for response in responses:
             if response is not None:
@@ -301,20 +273,20 @@ async def async_runner(base_url, headers):
     return results
 
 
-def mass_test(base_url, headers):
+def mass_test():
     customer_id = input("Enter the customer ID of the customer you want to test: ")
     test_nums = int(input("Enter the number of tests you want to run: "))
     test_interval = input("Enter the interval between tests in seconds: ")
     last_test_time = arrow.now("US/Mountain")
     last_test = None
-    results = []
     speedtest_url = f"{base_url}/networks/{customer_id}/speedtest"
     tests_done = 0
     while True:
         try:
             if tests_done < int(test_nums):
                 if arrow.now("UTC") > last_test_time.shift(seconds=int(test_interval)):
-                    if requests.get(speedtest_url, headers=headers, params={'limit': 1}).json()['data'][0]['date'] != last_test:
+                    if requests.get(speedtest_url, headers=headers,
+                                    params={'limit': 1}).json()['data'][0]['date'] != last_test:
                         last_test = requests.get(speedtest_url, headers=headers, params={'limit': 1})
                         last_test = last_test.json()
                         last_test_time = last_test['data'][0]['date']
@@ -324,9 +296,8 @@ def mass_test(base_url, headers):
                         last_test = last_test['data'][0]['date']
                         tests_done += 1
 
-
-        except Exception as e:
-            print(f"Error: {e}")
+        except Exception as err:
+            print(f"Error: {err}")
             continue
 
 
@@ -342,15 +313,12 @@ def main():
             choice = int(input("Enter your choice: "))
             if choice == 1:
                 print("Grabbing New Eero List")
-                asyncio.run(grab_eeros(base_url, headers))
+                asyncio.run(grab_eeros())
                 print("\n")
             elif choice == 2:
                 choice = input("Not fully implemented yet... Continue? (y/n): ")
                 if choice == 'y':
                     print("Too bad... Will crash... Not implemented yet")
-                #     print("Grabbing Full Eero Tests Results")
-                #     result = asyncio.run(grab_eeros(base_url, headers))
-                #     print(result)
                 else:
                     print("Exiting...")
                     break
@@ -362,27 +330,25 @@ def main():
                 if sub_choice == 1:
                     table = PrettyTable(['MAC', 'Serial', 'URL'])
                     mac = input("Enter the MAC address of the router you want to search for: ")
-                    url, serial = search_by_mac(base_url, headers, mac)
+                    url, serial = search_by_mac(mac)
                     url = f"https://dashboard.eero.com/networks/url"
                     table.add_row([mac, serial, url])
                     print(table)
                 elif sub_choice == 2:
                     print("Not working yet...")  # TODO
                 elif sub_choice == 3:
-                    result = asyncio.run(single_eero_results(base_url, headers, None))
+                    network_id = input("Enter the Eero network ID of the customer you want to test: ")
+                    result = asyncio.run(single_eero_results(network_id))
                     print(result)
-                    # print(single_eero_results(base_url, headers, None))
                 elif sub_choice == 0:
                     print("back to main menu")
                     break
             elif choice == 4:
                 print("Not working yet...")  # TODO
                 continue
-                # mass_test(base_url, headers)
             elif choice == 0:
                 print("Exiting")
                 break
         except TypeError:
             print("Invalid choice")
             continue
-
