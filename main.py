63fa0254d1867567ae8fca6323c3eec1be35c3e7
JTruj1ll0923 <jtrujillo923@gmail.com>
2022-04-08 13:07:17 -0600

    Fixed missing oui variable before printing Eero speed tests on single site check.

--- main.py
+++ main.py
@@ -546,7 +546,7 @@ def single_site_check():  # Return 0 if exiting single site check
         return 0
     mac = get_mac(None, ip)  # Get MAC address of connected NIC on target MBU
     if mac == '\'':  # Value that is returned if no MAC is found
-        url, serial, network_id = "N/A", "N/A", "N/A"
+        url, serial, network_id, oui = "N/A", "N/A", "N/A", "N/A"
     else:
         oui = MacLookup().lookup(mac)
         if oui == "eero inc.":
